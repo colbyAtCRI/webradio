eabdd3352678fbfb746c65cf1d1f5df9f0eaab6c
colbyAtCRI <paulccolby@earthlink.net>
2021-11-29 08:21:03 -0800

    fixed fms math

--- webradio/fmmodem.py
+++ webradio/fmmodem.py
@@ -159,7 +159,7 @@ class FMSModem(Modem):
         #
         Ap = sum(sig * self.kh19[:len(sig)])/len(sig)
         Ap = Ap/np.abs(Ap)
-        Bp = 2*Ap*self.kh38[:len(sig)].imag
+        Bp = 2*(Ap*self.kh38[:len(sig)]).imag
         sig_a = self.exit_decimation_a(self.deemphasis_a(sig))
         sig_b = self.exit_decimation_b(self.deemphasis_b(sig*Bp))
         pcm = np.zeros(2*len(sig_a))
