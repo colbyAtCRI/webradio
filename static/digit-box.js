0f41da6445e44d7177f81f0f9289de8611938198
Paul Colby <paulccolby@earthlink.net>
2021-12-02 08:44:57 -0800

    made into a class

--- static/digit-box.js
+++ static/digit-box.js
@@ -1,93 +1,98 @@
-function Digit(inc) 
+function newDiv(my_class)
 {
-   this.inc = inc;
-   this.elem = document.createElement('div');
-   this.elem.setAttribute('class','digit');
-   this.txt = document.createElement('div');
-   this.txt.innerHTML = '0';
-   this.txt.setAttribute('class','digit-text');
-   this.elem.appendChild(this.txt);
-   this.up = document.createElement('div');
-   this.up.setAttribute('class','top-cover');
-   this.elem.appendChild(this.up);
-   this.down = document.createElement('div');
-   this.down.setAttribute('class','bottom-cover');
-   this.elem.appendChild(this.down);
-   this.change = undefined;
-   this.up.onclick = function() 
-   {
-      if (this.change) 
-         this.change(this.inc);
-   }.bind(this);
-   this.down.onclick = function()
-   {
-      if (this.change)
-         this.change(-this.inc);
-   }.bind(this);
+    let elem = document.createElement('div');
+    elem.setAttribute('class',my_class);
+    return elem;
 }
 
-Digit.prototype.getValue = function()
+class Digit
 {
-   return this.inc*Number(this.txt.innerHTML);
-};
+    constructor(inc) {
+        this.inc = inc;
+        this.elem = newDiv('digit');
+        this.txt = newDiv('digit-text');
+        this.txt.innerHTML = '0';
+        this.elem.appendChild(this.txt);
+        this.up = newDiv('top-cover');
+        this.elem.appendChild(this.up);
+        this.down = newDiv('bottom-cover');
+        this.elem.appendChild(this.down);
+        this.change = undefined;
+        this.up.onclick = function()
+        {
+            if (this.change)
+                this.change(this.inc);
+        }.bind(this);
+        this.down.onclick = function() 
+        {
+            if (this.change)
+                this.change(-this.inc);
+        }.bind(this);
+    }
 
-Digit.prototype.setValue = function(value)
-{
-   this.txt.innerHTML = String(value);
-};
+    element() {
+        return this.elem;
+    }
 
-function DigitBox(element,ndigit)
-{
-   this.minValue = 0;
-   this.maxValue = 2000000000;
-   this.digits = [];
-   for (let n = 0; n < ndigit; n++) {
-      this.digits[n] = new Digit(Math.pow(10,n));
-   }
-   for (let n = 0; n < ndigit; n++) {
-      let k = ndigit - n - 1;
-      element.appendChild(this.digits[k].elem);
-      if ( (k + 1) % 3 === 0) {
-         this.digits[k].elem.style.borderLeft = '5px solid orange';
-      }
-   }
-   this.action = undefined;
-   this.change = function(inc)
-   {
-      let value = this.getValue();
-      value += inc;
-      if (value > this.minValue && value <= this.maxValue) {
-         this.setValue(value);
-         if (this.action)
-            this.action(value);
-      }
-   }.bind(this);
-   for (let n = 0; n < this.digits.length; n++)
-      this.digits[n].change = this.change;
+    getValue() {
+        return this.inc * Number(this.txt.innerHTML);
+    }
+
+    setValue(val) {
+        this.txt.innerHTML = String(val);
+    }
 }
 
-DigitBox.prototype.onchange = function(fnc)
+class DigitBox
 {
-   this.action = fnc;
+    minValue = 0;
+    maxValue = 2000000000;
+    digits = [];
+    action;
+
+    constructor(id,ndigit) {
+        let element = document.getElementById(id);
+        for (let n = 0; n < ndigit; n++) {
+            this.digits[n] = new Digit(Math.pow(10,n));
+        }
+        for (let n = 0; n < ndigit; n++) {
+            let k = ndigit - n - 1;
+            element.appendChild(this.digits[k].element());
+            if ((k + 1) % 3 === 0) 
+                this.digits[k].element().style.borderLeft = '5px solid orange';
+        }
+        this.change = function(inc)
+        {
+            let value = this.getValue();
+            value += inc;
+            if (value > this.minValue && value <= this.maxValue) {
+                if (this.action)
+                    this.action(value);
+            }
+        }.bind(this);
+        for (let digit of this.digits)
+            digit.change = this.change;
+    }
+
+    onchange(fnc) {
+        this.action = fnc;
+    }
+
+    getValue() {
+        let value = 0;
+        for (let digit of this.digits)
+            value += digit.getValue();
+        return value;
+    }
+
+    setValue(value) {
+        for (let digit of this.digits)
+            digit.setValue(0);
+        let str = String(Math.floor(value));
+        for (let n = 0; n < str.length; n++) {
+            let k = str.length - n - 1;
+            this.digits[k].setValue(Number(str[n]));
+        }
+    }
 }
-
-DigitBox.prototype.getValue = function()
-{
-   let value = 0;
-   for (let n = 0; n < this.digits.length; n++) {
-      value += this.digits[n].getValue();
-   }
-   return value;
-};
-
-DigitBox.prototype.setValue = function(value)
-{
-   for (let n = 0; n < this.digits.length; n++)
-      this.digits[n].setValue(0);
-   let str = String(Math.floor(value));
-   for (let n = 0; n < str.length; n++) {
-      let k = str.length - n - 1;
-      this.digits[k].setValue(Number(str[n]));
-   }
-};
 
