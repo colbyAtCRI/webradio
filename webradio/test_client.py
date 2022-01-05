05a17dd27ad2a2c8899780f453e553848f135342
Paul Colby <paulccolby@earthlink.net>
2022-01-05 15:14:16 -0800

    make runable

--- webradio/test_client.py
+++ webradio/test_client.py
@@ -1,7 +1,7 @@
 import numpy as np
 import socketio
 from struct import unpack
-from webradio.javadict import ChangeOrder, toCO
+from javadict import ChangeOrder, toCO
 
 # from matplotlib import pyplot as plt
 
